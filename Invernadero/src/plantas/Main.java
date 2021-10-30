5400a7d48bd7fb6bd1ff060f5cbab8d4e186c293
Simon-Barrios-DiS <93356504+Simon-Barrios-DiS@users.noreply.github.com>
2021-10-30 14:03:55 -0400

    Update Main.java

--- Invernadero/src/plantas/Main.java
+++ Invernadero/src/plantas/Main.java
@@ -4,7 +4,18 @@ public abstract class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Todas las plantas son plantas inventandas para fines de muestra
+		Musgos Mus1= new Musgos("Musgero", "Musgerus", 10.9, 10, false, "Briofritas Musgos", false, "Musgo de agua");
+		System.out.println(Mus1);
+		
+		Magnoliidae Mag1= new Magnoliidae("Magnolia", "Magnolius", 12.0, 8, false, "Angiospermas Magnoliidae", "Flores", "Decoracion");
+		System.out.println(Mag1);
 
+		Gnetidae Gne1= new Gnetidae("Gneto", "Gnetus", 27.2, 19, false, "Gumnospermas Gnetidae", "Conos", "Asia");
+		System.out.println(Gne1);
+
+		Isoetes Iso1= new Isoetes("Isobel", "Isobelus", 3.1, 15, true, "Pteridofitas Isoetes", false, false);
+		System.out.println(Iso1);
 	}
 
 }
